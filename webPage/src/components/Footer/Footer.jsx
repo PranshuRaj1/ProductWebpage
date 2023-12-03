3c054db4f921b6f34a4e711d956d982af625ff68
Pranshu Raj <139574633+PranshuRaj1@users.noreply.github.com>
2023-12-03 22:52:32 +0530

    Update Footer.jsx

--- webPage/src/components/Footer/Footer.jsx
+++ webPage/src/components/Footer/Footer.jsx
@@ -83,8 +83,11 @@ export default function Footer() {
         <div className="sm:flex sm:items-center sm:justify-between">
           <span className="text-sm text-gray-500 sm:text-center">
             © 2023
-            <a href="https://hiteshchoudhary.com/" className="hover:underline">
-              hiteshchoudhary
+            <a
+              href="https://twitter.com/RajPranshu12"
+              className="hover:underline"
+            >
+              Pranshu Raj
             </a>
             . All Rights Reserved.
           </span>
